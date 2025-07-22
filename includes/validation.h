ecc148fed1d4c773c4367ef473beaf1b23970c10
simy404 <asimetra404@gmail.com>
2025-07-22 22:35:03 +0200

    add: error makro

--- includes/validation.h
+++ includes/validation.h
@@ -6,7 +6,7 @@
 /*   By: hsamir <hsamir@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 13:58:25 by sdaban            #+#    #+#             */
-/*   Updated: 2025/07/22 19:00:37 by hsamir           ###   ########.fr       */
+/*   Updated: 2025/07/22 22:33:22 by hsamir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,33 @@ typedef struct s_result
 
 }			t_result;
 
+#define USAGE_ERR "Usage: ./cub3D <name>.cub"
+#define INVALID_EXT_ERR "Invalid file extension"
+#define INVALID_FILE_ERR "Invalid <name>.cub file"
+#define MISSING_ERR "missing elements in file"
+#define INVALID_ELEMENT_ERR "invalid element identifier"
+
+#define DUP_TEXTURE_ERR "duplicate texture"
+#define EMPTY_TEXTURE_ERR "empty texture path"
+#define INVALID_TEXTURE_ERR "invalid texture path"
+
+#define DUP_COLOR_ERR "duplicate color"
+
+#define MAP_ERR "map is not last element"
+#define DUP_PLAYER_ERR "duplicate player"
+#define MAP_PLAYER_ERR "invalid player count on map line"
+
+
 bool		v_color_value(int value);
 bool		v_map();
 bool		v_fc_colors(int floor, int ceiling); // floor and ceiling
 bool		v_directions();
 
 int			is_valid_file_extension(char *file_path);
+int			is_valid_player_count(char *input);
 
-t_result	validate_color(t_element element, int seen_mask);
-t_result	validate_texture(t_element element, int seen_mask);
-t_result	validate_map(t_element *elements, t_element element, int seen_mask);
+t_result	validate_color(t_element *element, int seen_mask);
+t_result	validate_texture(t_element *element, int seen_mask);
+t_result	validate_map(t_element *element, int seen_mask);
 
 #endif
