444024f5844e4bf091e8d7a9941937eb07bf43be
Salih Umut Daban <sdaban@k1m21s03.42kocaeli.com.tr>
2025-08-06 14:41:51 +0300

    basic changes

--- includes/config.h
+++ includes/config.h
@@ -6,7 +6,7 @@
 /*   By: sdaban <sdaban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 13:43:59 by hsamir            #+#    #+#             */
-/*   Updated: 2025/08/06 10:38:05 by sdaban           ###   ########.fr       */
+/*   Updated: 2025/08/06 14:38:33 by sdaban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 # define CONFIG_H
 
 # define TITLE "Cub3D"
-# define WIDTH 720
-# define HEIGHT 600
+# define WIDTH 1200
+# define HEIGHT 800
 # define FOV 66
-# define SPEED 0.05f
+# define SPEED 0.06f
 # define ROTATION_ANGLE 0.04f
 
 #endif
